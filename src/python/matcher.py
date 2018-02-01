0b5462d55aff6403a97da73af40c8ebffaed1c23
kam6512 <kam6512@gmail.com>
2018-02-01 17:59:09 +0900

    matcher.py test

--- src/python/matcher.py
+++ src/python/matcher.py
@@ -1,14 +1,33 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-img_rgb = cv.imread(‘menu.png’)
+
+lineColor = (0,0,255)
+thickness = 1
+
+img_rgb = cv.imread(""".\samples\\menu.png""")
 img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-template = cv.imread(‘icon.png’,0)
+template = cv.imread(""".\samples\\test1.png""",0)
 w, h = template.shape[::-1]
 res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
-threshold = 0.8
+threshold = 0.95
 loc = np.where( res >= threshold)
+print(loc)
 for pt in zip(*loc[::-1]):
-#    cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-   print(pt)
-# cv.imwrite(‘res.png’,img_rgb)
+   start = pt
+   end = (pt[0] + w, pt[1] + h)
+   cv.rectangle(img_rgb, start, end, lineColor, thickness)
+   break
+
+cv.imwrite(""".\samples\\res.png""",img_rgb)
+pointX = ((start[0]+end[0])/2)
+pointY = ((start[1]+end[1])/2)
+
+print(start)
+print(end)
+print(pointX)
+print(pointY)
+
+
+
+#npm --add-python-to-path='true' --debug install --global windows-build-tools
