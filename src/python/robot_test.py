bfffb505248b23609961c0019a76c147c22c1e88
kam6512 <kam6512@gmail.com>
2018-02-02 17:49:16 +0900

    add method

--- src/python/robot_test.py
+++ src/python/robot_test.py
@@ -2,16 +2,20 @@ import uuid
 import pyautogui
 import matcher
 
-screenShot = '.\\samples\\'
-screenShot +=(uuid.uuid4().hex)
-screenShot +='.png'
-pyautogui.screenshot(screenShot)
+
+def clickNow(matchImage):
+    screenShot = '.\\samples\\'
+    screenShot += (uuid.uuid4().hex)
+    screenShot += '.png'
+    pyautogui.screenshot(screenShot)
+
+    (pointX, pointY) = matcher.calcCenterPoint(
+        screenShot,  matchImage)
+
+    print(pointX)
+    print(pointY)
+    pyautogui.moveTo(pointX, pointY)
+    pyautogui.click()
 
 
-(pointX, pointY) = matcher.calcCenterPoint(
-   screenShot,   """.\samples\\test2.png""")
-
-print(pointX)
-print(pointY)
-pyautogui.moveTo(pointX, pointY)
-pyautogui.click()
+clickNow(""".\samples\\test2.png""")
