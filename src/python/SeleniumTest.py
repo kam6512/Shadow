86fbe846d89acf7f918b114fda0f0538764d968f
kam6512 <kam6512@gmail.com>
2018-02-06 14:44:00 +0900

    use  DaumMap Extension

--- src/python/SeleniumTest.py
+++ src/python/SeleniumTest.py
@@ -1,11 +1,6 @@
 import os
 import time
 import pyautogui
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 from extension import DaumMap
 
 from util import Scanner
