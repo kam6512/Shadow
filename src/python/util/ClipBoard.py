b3de2a8509675fc3ee3918f0502255dd72f4c6e5
kam6512 <kam6512@gmail.com>
2018-02-06 15:04:00 +0900

    update ClipBoard Code

--- src/python/util/ClipBoard.py
+++ src/python/util/ClipBoard.py
@@ -1,14 +1,24 @@
 import win32clipboard
 
-def getClipboardData():
-    win32clipboard.OpenClipboard()
-    data = win32clipboard.GetClipboardData()
-    win32clipboard.CloseClipboard()
-    return data
 
+class Clip:
+    def getClipboardData(self):
+        self.open()
+        data = win32clipboard.GetClipboardData()
+        self.close
+        return data
 
-def setClipboard(text):
-    win32clipboard.OpenClipboard()
-    win32clipboard.EmptyClipboard()
-    win32clipboard.SetClipboardData(win32clipboard.CF_UNICODETEXT, text)
-    win32clipboard.CloseClipboard()
+    def setClipboard(self, text):
+        self.open()
+        self.clean()
+        win32clipboard.SetClipboardData(win32clipboard.CF_UNICODETEXT, text)
+        self.close
+
+    def open(self):
+        win32clipboard.OpenClipboard()
+
+    def clean(self):
+        win32clipboard.EmptyClipboard()
+
+    def close(self):
+        win32clipboard.CloseClipboard()
