488cf0d8c826a30eb8e11c123f05554437ddf567
kam6512 <kam6512@gmail.com>
2018-02-07 19:32:14 +0900

    ocr test with cv2

---
+++ src/python/util/PyTesseract.py
@@ -0,0 +1,13 @@
+try:
+    import Image
+except ImportError:
+    from PIL import Image
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\DEV_MODULE\\Tesseract-OCR\\tesseract.exe'
+def OCR(path, language = 'eng'):
+    image = Image.open(path)
+    text = pytesseract.image_to_string(image, lang=language)
+    print(text)
+
+# OCR('..\samples\OCR\\2.png')
