6fb9dd1badb94aa4b4eac819be2065984b32eb9c
강영원 <kam6512@gmail.com>
2018-02-08 00:56:34 +0900

    fix recall

--- src/python/util/PyTesseract.py
+++ src/python/util/PyTesseract.py
@@ -6,13 +6,12 @@ except ImportError:
     from PIL import Image
 import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = 'C:\\DEV_MODULE\\Tesseract-OCR\\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = 'C:\DEV\MODULES\Tesseract-OCR\\tesseract.exe'
 
 
 def ocr(path):
     image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     image = cv2.pyrUp(image)
-    ocr(image)
     cv2.imwrite('..\\samples\\upscale.png', image)
     text = pytesseract.image_to_string(image, lang='eng', config="-psm 7 -oem 3")
     print(text)
